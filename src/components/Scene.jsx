8f03977de813c952a02cdecf40144b5364f68efc
“JEREMY <Jeremyinoa67@gmail.com>
2024-04-26 21:00:45 -0400

    add custom textures to meshes also practice for double side and position

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,32 +1,20 @@
-import { useFrame, extend, useThree } from "@react-three/fiber"
+import { useFrame, extend, useThree, useLoader } from "@react-three/fiber"
 import { useRef } from "react"
 import * as THREE from 'three'
 import { OrbitControls } from "@react-three/drei";
-import CustomComp from "./CustomComp";
 extend({OrbitControls:OrbitControls});
 
 const Scene = (props) => {
-    const cubeRef = useRef();
-    const planeRef = useRef();
-    const { gl, camera } = useThree();
-    
-
-    useFrame((state,delta)=>{
-        // cubeRef.current.rotation.x += delta;
-        // cubeRef.current.rotation.y += delta;
-        // planeRef.current.rotation.x += delta;
-        // planeRef.current.rotation.y += delta;
-        // state.camera.position.x = Math.sin(state.clock.elapsedTime)
-    })
+    const texture = useLoader(THREE.TextureLoader,'./public/trunk.jpeg');
 
     return (
         <>
         <OrbitControls/>
-        <mesh >
-            <boxGeometry  />
-            <meshBasicMaterial color={'orange'}/>
+        <mesh position-y={2}>
+            <planeGeometry args={[4,4]} />
+            <meshBasicMaterial  map={texture} side={THREE.DoubleSide}/>
         </mesh>
-        <CustomComp/>
+
         </>
     )
 }

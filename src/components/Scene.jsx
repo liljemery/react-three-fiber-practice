c6e72450d40ae7e1f08a838d3addda37088b0c41
“JEREMY <Jeremyinoa67@gmail.com>
2024-04-28 19:45:19 -0400

    cube Camera to make objects reflect environment

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,55 +1,42 @@
-import { OrbitControls, useHelper, Sparkles, Stars, Cloud, Sky, Environment, Lightformer } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Environment, CubeCamera } from "@react-three/drei";
 import * as THREE from 'three'
 import { useRef } from 'react'
 import { useControls } from 'leva'
+import {useFrame } from '@react-three/fiber'
+
 
 const Scene = (props) => {
-    const sky = useRef()
-    const { sunPosition } = useControls('sky',{
-        sunPosition:{value:[0,1,0]}
-    })
+    const cube = useRef()
     const { height, radius, scale } = useControls('ground',{
         height: 6, 
         radius: 60, 
         scale: 70
     })
+    useFrame((_,delta)=>{
+        cube.current.rotation.x += delta;
+        cube.current.rotation.z += delta;
+    })
+    
     return (
         <>
         <OrbitControls/>
-        {/* <ambientLight
-            
-        /> */}
-        {/* <directionalLight 
-            castShadow
-            position={sunPosition}
-        /> */}
-
-        {/* <Sparkles scale={5} count={1000000} speed={0.2} opacity={3} color={'white'} />*/}
-        {/* <Stars />
-        <Cloud opacity={1} speed={0.2} width={20} depth={1.5} segments={40} /> */}
-        {/* <Sky
-            ref={sky}
-            sunPosition={sunPosition}
-            castShadow
-        /> */}
         < Environment
-            files={'./envMap/1.hdr'}
+            files={'./envMap/2.hdr'}
             ground={{height, radius, scale}}
-        >
-            <mesh>
-                <planeGeometry/>
-                <meshBasicMaterial/>                
-            </mesh>
-            <Lightformer />
-        </Environment>
-        <mesh castShadow position-y={1}>
+        />
+        <CubeCamera>
+            {(texture) => (
+                <mesh position-y={3}>
+                    <sphereGeometry args={[1,64,64]}/>
+                    <meshStandardMaterial envMap={texture} roughness={0} metalness={1} />
+                </mesh>   
+            )}  
+        </CubeCamera>
+        <mesh ref={cube} position-z={2}>
             <boxGeometry/>
-            <meshStandardMaterial color={'#C7CAC7'}  />
+            <meshBasicMaterial color={'purple'}/>
         </mesh>
-        <mesh receiveShadow={true}  position-y={0} rotation-x={-Math.PI * 0.5}>
-            <planeGeometry args={[8,8]} />
-            <meshStandardMaterial color={"#CC3941"} side={THREE.DoubleSide}/>
-        </mesh>
+        {/* <PerspectiveCamera makeDefault position={[0,0,5]} fov={75}/> */}
         </>
     )
 }

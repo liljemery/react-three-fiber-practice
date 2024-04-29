3b55802277f410179cf87bffa0bc70f11a9ead1e
“JEREMY <Jeremyinoa67@gmail.com>
2024-04-29 18:39:05 -0400

    cameraControls rotation by leva menu

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,42 +1,44 @@
-import { OrbitControls, PerspectiveCamera, Environment, CubeCamera } from "@react-three/drei";
-import * as THREE from 'three'
+import { Grid, OrbitControls, CameraControls } from "@react-three/drei";
 import { useRef } from 'react'
-import { useControls } from 'leva'
-import {useFrame } from '@react-three/fiber'
-
+import { button, buttonGroup, useControls } from 'leva';
+import * as THREE from 'three'
 
 const Scene = (props) => {
-    const cube = useRef()
-    const { height, radius, scale } = useControls('ground',{
-        height: 6, 
-        radius: 60, 
-        scale: 70
-    })
-    useFrame((_,delta)=>{
-        cube.current.rotation.x += delta;
-        cube.current.rotation.z += delta;
+    const CameraControlRef = useRef();
+    const { DEG2RAD } =THREE.MathUtils;
+
+    const cameraControls = useControls("Camera Controls",{
+        horizontalRotation:buttonGroup({
+            label: "Horizontal R",
+            opts:{
+                "45deg":()=> CameraControlRef.current.rotate(75 * DEG2RAD ,0,true),
+                "90deg":()=> CameraControlRef.current.rotate(90 * DEG2RAD ,0,true),
+                "360deg":()=> CameraControlRef.current.rotate(360 * DEG2RAD ,0,true)
+            },
+        }),
+        verticalRotation:buttonGroup({
+            label: "Vertical R",
+            opts:{
+                "20deg":()=> CameraControlRef.current.rotate(0,20 * DEG2RAD ,true),
+                "-40deg":()=> CameraControlRef.current.rotate(0,-40 * DEG2RAD ,true),
+            },
+        }),
     })
     
     return (
         <>
-        <OrbitControls/>
-        < Environment
-            files={'./envMap/2.hdr'}
-            ground={{height, radius, scale}}
-        />
-        <CubeCamera>
-            {(texture) => (
-                <mesh position-y={3}>
-                    <sphereGeometry args={[1,64,64]}/>
-                    <meshStandardMaterial envMap={texture} roughness={0} metalness={1} />
-                </mesh>   
-            )}  
-        </CubeCamera>
-        <mesh ref={cube} position-z={2}>
-            <boxGeometry/>
-            <meshBasicMaterial color={'purple'}/>
-        </mesh>
-        {/* <PerspectiveCamera makeDefault position={[0,0,5]} fov={75}/> */}
+            <CameraControls ref={CameraControlRef} smoothTime={0.5}/>
+
+            <Grid 
+                args={[30,30]} 
+                cellSize={0.25} 
+                sectionSize={2} 
+                sectionThickness={1.5} 
+            />
+            <mesh>
+                <boxGeometry/>
+                <meshBasicMaterial />
+            </mesh>
         </>
     )
 }

0981db487e04a6160297dfb9a58a9f3468f899bd
“JEREMY <Jeremyinoa67@gmail.com>
2024-04-29 18:46:41 -0400

    leva controls camera truck ( move in space (not rotation) and zoom method

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -8,7 +8,7 @@ const Scene = (props) => {
     const { DEG2RAD } =THREE.MathUtils;
 
     const cameraControls = useControls("Camera Controls",{
-        horizontalRotation:buttonGroup({
+        horizontalRotation: buttonGroup({
             label: "Horizontal R",
             opts:{
                 "45deg":()=> CameraControlRef.current.rotate(75 * DEG2RAD ,0,true),
@@ -16,11 +16,26 @@ const Scene = (props) => {
                 "360deg":()=> CameraControlRef.current.rotate(360 * DEG2RAD ,0,true)
             },
         }),
-        verticalRotation:buttonGroup({
+        verticalRotation: buttonGroup({
             label: "Vertical R",
             opts:{
                 "20deg":()=> CameraControlRef.current.rotate(0,20 * DEG2RAD ,true),
                 "-40deg":()=> CameraControlRef.current.rotate(0,-40 * DEG2RAD ,true),
+            },
+        }),
+        truckGroup: buttonGroup({
+            label: "truck R",
+            opts:{
+                "(1,0)":()=> CameraControlRef.current.truck(1,0,true),
+                "(0,+1)":()=> CameraControlRef.current.truck(0,+1,true),
+                "(-1,-1)":()=> CameraControlRef.current.truck(-1,-1,true),
+            },
+        }),
+        zoomGroup: buttonGroup({
+            label: "Zoom",
+            opts:{
+                0.25 :()=> CameraControlRef.current.zoom(0.25,true),
+                "-0.25":()=> CameraControlRef.current.zoom(-0.25 ,true),
             },
         }),
     })

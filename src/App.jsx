c766152ad0ee4cd35069a1238072729a59ce9ec1
“JEREMY <Jeremyinoa67@gmail.com>
2024-05-01 21:02:44 -0400

    spring values changing between animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 import { Perf } from "r3f-perf";
 
+
 function App() {
   return (
-    <Canvas shadows camera={{position: [0,0,5], fov: 60}}>
+    <Canvas shadows camera={{position: [0,1,5], fov: 60}}>
       <Scene />
       <Perf position={'top-left'}/>
     </Canvas>

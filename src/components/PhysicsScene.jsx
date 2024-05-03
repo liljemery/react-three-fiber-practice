0c497205f152a797163e5ae95e607a5dbc6c502a
“JEREMY <Jeremyinoa67@gmail.com>
2024-05-02 21:57:14 -0400

    add physics and gravity to models

--- src/components/PhysicsScene.jsx
+++ src/components/PhysicsScene.jsx
@@ -1,13 +1,20 @@
-import { Physics, RigidBody } from "@react-three/rapier";
+import { Physics, RigidBody, } from "@react-three/rapier";
 
 const PhysicsScene = () => {
-    const grav = -9.81
+
   return (
-    <Physics gravity={[0,grav,0]}>
-        <RigidBody>
-            <mesh castShadow position={[0, 10, 0]}>
+    <Physics gravity={[0,-9.81,0]} debug>
+        <RigidBody wireframe>
+            <mesh castShadow position={[0, 1.5, 0]}>
                 <boxGeometry />
                 <meshStandardMaterial color="#CC3941" />
+            </mesh>            
+        </RigidBody>
+        {/* trimesh or hull for complex structures */}
+        <RigidBody  colliders='hull'>
+            <mesh position={[-1.5,1.5,0]} castShadow>
+                <torusKnotGeometry args={[0.5,0.15,100,100]}/>
+                <meshStandardMaterial color={'orange'}/>
             </mesh>            
         </RigidBody>
         <RigidBody type="fixed" >
